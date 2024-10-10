73c6c08970d61d920ef2934116e4f302d9bde6ee
Вячеслав <slava.kbem@mail.ru>
2024-10-10 13:17:42 +0300

    Сложение

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Капралов324
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, result, result2,result3,result4;
+            Console.WriteLine("1 число");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2 число");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Сложение");
+            result = a + b;
+
+        }
+    }
+}
