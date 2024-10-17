568af4db22231bc04c85ed6de435fb35d3990d6a
Вячеслав <slava.kbem@mail.ru>
2024-10-17 13:34:44 +0300

    PrRmoyg

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Капралов324
         static void Main(string[] args)
         {
             int a, b, result, result2,result3,result4;
-            double resultR,r;
+            double resultR,r,r2;
             Console.WriteLine("1 число");
             a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("2 число");
@@ -31,6 +31,10 @@ namespace Капралов324
             resultR = Math.PI*(r*2);
             Console.WriteLine("Ответ: " + resultR);
             resultR = r*r;
+            Console.WriteLine("Ответ: " + resultR);
+            Console.WriteLine("Вторую сторону");
+            r2 = Convert.ToDouble(Console.ReadLine());
+            resultR = Math.PI * (r2 * 2);
             Console.WriteLine("Ответ: " + resultR);
 
 
